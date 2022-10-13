a3ad7eaac19e809051de2322a7f22db0433a4286
Andres Brito <robin_brito@hotmail.com>
2022-10-13 14:11:40 -0500

    ejercicio 2

--- main.js
+++ main.js
@@ -14,10 +14,25 @@ select.addEventListener('change',() =>{
             text.value = x.text
         }
     }) */
+    //indice del select
     number.value = select.selectedIndex
+    //texte del select
     text.value = select.options[select.selectedIndex].text; 
+    //numero del color
     colorValue.value = select.value
+    //pintar color
     color.value = `#${select.value}`
 })
 
 //ejercicio 2
+
+const textArea = $('#description')
+const btnTextArea= $('#btnTextArea')
+
+btnTextArea.addEventListener('click', ()=>{
+    if(textArea.value.length>50){
+        alert('Tiene mas de 50 caracteres')
+    }else alert('Tiene menos de 50 caracteres')
+})
+
+//ejercicio 3
